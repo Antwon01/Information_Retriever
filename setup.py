d4a44201c44191935978afd1d14bb97e599a07ed
Antwon01 <jake3767mn@gmail.com>
2024-10-24 16:34:00 -0500

    finished setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Projects",
+    version = "0.1",
+    author = "Anthony Montelongo-Navejar",
+    author_email = "jake3767mn@gmail.com",
+    packages = find_packages(),    
+    install_requires = []
+
+)
